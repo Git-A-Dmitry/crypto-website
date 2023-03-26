cde8764475a0e86284f502f6d7178d539844e759
Dmitry A <adcntt@gmail.com>
2023-03-27 00:51:08 +0300

    header, hero

--- components/Header.js
+++ components/Header.js
@@ -1,36 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
 import Link from 'next/link';
+// import styles from '@/styles/Header.module.scss';
+import { FaBars, FaTimes } from 'react-icons/fa';
+
+const navigation = [
+  { id: 1, title: 'Home', path: '/' },
+  { id: 2, title: 'Crypto', path: '/crypto' },
+  { id: 3, title: 'About', path: '/about' },
+];
 
 const Header = () => {
+  const { pathname } = useRouter();
+  const [menu, setMenu] = useState(false);
+
+  const handleClick = () => setMenu(!menu);
+
   return (
     <div>
-      <header>
-        {/* <div className='container header'> */}
-        <div className='header'>
-          <div className='logo'>
-            {/* <h3>crypto</h3> */}
-            {/* <h2>
+      <header className='header'>
+        <nav className='navbar'>
+          {/* <div className='logo'> */}
+          {/* <h3>crypto</h3> */}
+          {/* <h2>
               Crypto <span className='--color-danger'>X</span>
             </h2> */}
+          {/* </div> */}
+
+          <div className='navbar__hamburger' onClick={handleClick}>
+            {menu ? (
+              <FaTimes size={24} style={{ color: 'ffffff' }} /> //
+            ) : (
+              <FaBars size={24} style={{ color: 'ffffff' }} />
+            )}
           </div>
 
-          <nav>
-            <ul>
-              <li>
-                <Link href={'/'}>Home</Link>
-              </li>
-              <li>
-                <Link href={'/crypto'}>Crypto</Link>
-              </li>
-              <li>
-                <Link href={'/about'}>About</Link>
-              </li>
-              <li className='--btn --btn-primary'>
+          <div className={menu ? 'navbar__nav-menu active' : 'navbar__nav-menu'}>
+            {navigation.map(({ id, title, path }) => (
+              <Link //
+                key={id}
+                href={path}
+                className={pathname === path ? 'active' : null}
+              >
+                {title}
+              </Link>
+            ))}
+          </div>
+
+          {/* <li className='--btn --btn-primary'>
                 <Link href={'/'}>Get Started</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
+              </li> */}
+        </nav>
       </header>
     </div>
   );
--- components/Hero.js
+++ components/Hero.js
@@ -1,14 +1,30 @@
 import Image from 'next/image';
 import styles from '@/styles/Hero.module.scss';
+import { motion } from 'framer-motion';
 
 const Hero = () => {
   return (
     <section className={styles.hero}>
       <div className='container'>
         <div className={styles.text}>
-          <h1>Discover the World of Cryptocurrency</h1>
+          <motion.h1 //
+            animate={{ scale: 1, opacity: 1 }}
+            initial={{ scale: 0.9, opacity: 0.5 }}
+            transition={{ type: 'tween', duration: 5 }}
+          >
+            Discover the World of
+          </motion.h1>
+
+          <motion.h1 //
+            animate={{ scale: 1, opacity: 1 }}
+            initial={{ scale: 0.9, opacity: 0 }}
+            transition={{ type: 'tween', duration: 5 }}
+          >
+            Cryptocurrency
+          </motion.h1>
+
           <p>Are you curious about the exciting and rapidly-evolving world of cryptocurrency? Look no further! Our website is the perfect resource for anyone interested in learning more about this innovative form of digital currency.</p>
-          <a href='crypto' className='--btn --btn-hero'>
+          <a href='crypto' className='btn btn-hero'>
             Get Started
           </a>
         </div>
